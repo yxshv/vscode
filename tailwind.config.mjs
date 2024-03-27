a4280264b1e838e0108dc415bce0e16fcfb2c970
Yash <yashverma30115@gmail.com>
2024-03-27 08:21:21 +0000

    create navbar

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        "noto-sans": ["Noto Sans", "sans-serif"],
+      },
+      colors: {
+        bg: "#1D1E20",
+        text: {
+          1: "#E9EBF4",
+          2: "#B2B4BE",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
