dd074af9541b05a84a6cc42751c8e68b8d124d28
Yash <yashverma30115@gmail.com>
2024-03-29 07:59:45 +0000

    extensions

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -24,7 +24,7 @@ export default {
       },
     },
     animation: {
-      "fade-in": "fade-in 1s ease-in-out",
+      "fade-in": "fade-in 1s ease-in-out forwards",
     },
     keyframes: {
       "fade-in": {
